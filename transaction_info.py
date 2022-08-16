8a0458d61734391bb07544f5e243473b078c3181
rjaconski <rjaconski1@gmail.com>
2022-08-16 13:19:02 -0400

    Update transaction_info.py

--- transaction_info.py
+++ transaction_info.py
@@ -1,26 +1,30 @@
 import random
 
-
 def main():
-    count = 0
-    while count < 15000:
-        payment_type = [ "Credit Card", "Venmo", "Google pay", "Apple pay", "Cash app", "Gift card", "Paypal"]
-        fail_reason_card1 = ["Failed to Authenticate buyer","Card reported stolen", "Insuffecient Funds", "Incorrect CCV", "Card Type Not Supported"]
-        fail_reason_card2 = ["Insuffecient Funds", "Incorrect CCV", "Gift Card Type Not Supported"]
-        fail_reason_app = ["Failed to Authenticate buyer", "Payment site timed out", "Insuffecient Funds"]
-        payment_info = []
-        payment_info.append(random.choice(payment_type))
+    trans = transactionTest() 
+    paymentType = trans[0]
+    trans.remove(trans[0])
+    print(paymentType, ":",trans)
+def transactionTest():
+        
+    
+    payment_type = [ "Credit Card", "Venmo", "Google pay", "Apple pay", "Cash app", "Gift card", "Paypal"]
+    fail_reason_card1 = ["Failed to Authenticate buyer","Card reported stolen", "Insuffecient Funds", "Incorrect CCV", "Card Type Not Supported"]
+    fail_reason_card2 = ["Insuffecient Funds", "Incorrect CCV", "Gift Card Type Not Supported"]
+    fail_reason_app = ["Failed to Authenticate buyer", "Payment site timed out", "Insuffecient Funds"]
+    payment_info = []
+    payment_info.append(random.choice(payment_type))
 
-        if random.randint(1,100) > 50:
-            payment_info.append("Y")
+    if random.randint(1,100) > 50:
+        payment_info.append("Y")
+    else:
+        payment_info.append("N")
+        if payment_info[0] == "Credit Card":
+            payment_info.append(random.choice(fail_reason_card1))
+        elif payment_info[0] == "Gift Card":
+            payment_info.append(random.choice(fail_reason_card2))
         else:
-            payment_info.append("N")
-            if payment_info[0] == "Credit Card":
-                payment_info.append(random.choice(fail_reason_card1))
-            elif payment_info[0] == "Gift Card":
-                payment_info.append(random.choice(fail_reason_card2))
-            else:
-                payment_info.append(random.choice(fail_reason_app))        
-        count += 1
-        print(payment_info)
+            payment_info.append(random.choice(fail_reason_app))        
+    
+    return payment_info
 main()
